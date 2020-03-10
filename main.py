bc7bb251966227960b817b20827f0dda776354a2
jobpasin <jobpasin@hotmail.com>
2020-03-10 18:13:02 +0900

    Added visualization (Prototype)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from model import InceptionV3
-import os,sys
+import os, sys
 import linecache
 import numpy as np
 from lime import lime_image
@@ -7,17 +7,29 @@ from tensorflow.keras.applications import inception_v3 as inc_net
 from tensorflow.keras.applications.inception_v3 import preprocess_input, decode_predictions
 from tensorflow.keras.preprocessing import image
 from skimage.segmentation import mark_boundaries
+import matplotlib.pyplot as plt
+import time
+
+
+def get_imagenet_to_label():
+    imagenet_code_to_label = {}
+    with open(".\imagenet_to_label.txt") as f:
+        lines = f.readlines()
+
+    for line in lines:
+        temp = line.replace('{', '').replace('}', '').split(':')
+        imagenet_code_to_label[int(temp[0])] = temp[1].replace('\'', '').strip()
+    return imagenet_code_to_label
+
 
 if __name__ == '__main__':
     model = inc_net.InceptionV3()
+    model.summary()
 
-    explainer = lime_image.LimeImageExplainer()
 
-
-    img = image.load_img('./data/automobile1.png', target_size=(299,299))
-    print(img)
+    img = image.load_img('./data/bed2.jpg', target_size=(299, 299))
     x = image.img_to_array(img)
-    x = np.expand_dims(x,axis=0)
+    x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
     x = np.vstack([x])
 
@@ -26,5 +38,17 @@ if __name__ == '__main__':
     for i in decode_predictions(prediction):
         print(i)
 
-    explanation = explainer.explain_instance(x[0], model.predict, top_labels=5,num_samples=1000)
+    start = time.time()
+    explainer = lime_image.LimeImageExplainer()
+    explanation = explainer.explain_instance(x[0], model.predict, top_labels=5, hide_color=0, num_samples=1000)
     print(explanation)
+
+    x = 0
+    decoder = get_imagenet_to_label()
+    print(explanation.top_labels[x], decoder[explanation.top_labels[x]])
+
+    temp, mask = explanation.get_image_and_mask(explanation.top_labels[x], positive_only=False, num_features=10,
+                                                hide_rest=False)
+    print("Explanation time", time.time() - start)
+    plt.imshow(mark_boundaries(temp / 2 + 0.5, mask))
+    plt.show()
