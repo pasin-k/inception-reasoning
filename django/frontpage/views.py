b1bf159905405389764cf090763e47d3c6525e98
jobpasin <jobpasin@hotmail.com>
2020-04-24 02:54:12 +0900

    Need to move lime-algorithm inside django. Change project name. Implement lime in views. Not tested yet

--- django/frontpage/views.py
+++ django/frontpage/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView # new
+from django.views.generic import ListView # new
 from django.urls import reverse_lazy # new
 from django.conf import settings
 
 from pathlib import Path
-import os
+import os, sys
 from PIL import Image as PILImg
 from PIL import ImageEnhance as PILImageEnhance
-from .forms import UploadForm # new
+from .forms import UploadForm
 from .models import Upload
 
+sys.path.append('..')
+from algorithm.main import predict
+
+# Create your views here.
 class HomePageView(ListView):
     model = Upload
     template_name = 'home.html'
@@ -17,53 +21,42 @@ class HomePageView(ListView):
 
 def upload_file(request):
     template = "uploads.html"
-    folder = "images"
+    folder = "images/"
     data = {}
     if request.POST:
         userform = UploadForm(request.POST, request.FILES)
-        print(userform)
         if userform.is_valid():
+            #TODO: Connect this part with our model
             origin_form = userform.cleaned_data["user_file"]
-            origin_name = Path(folder) / origin_form.name
-            original_file = Path(settings.MEDIA_ROOT).joinpath(origin_name)
-            thumb_name = Path(folder) / (original_file.stem + "_thumb.jpg")
-            thumb_file = Path(settings.MEDIA_ROOT).joinpath(thumb_name)
+            origin_name = origin_form.name
+            original_file = Path(settings.MEDIA_ROOT).joinpath(folder, origin_name)
+            predicted_name = original_file.stem + "_prediction.png"
+            prediction_file = Path(settings.MEDIA_ROOT).joinpath(folder, predicted_name)
             if original_file.is_file():
                 original_file.unlink()
-            if thumb_file.is_file():
-                thumb_file.unlink()
-            with open(original_file, 'wb+') as f:
-                f.write(origin_form.read())
-            origin_form.seek(0)
-            # resize image
+            if prediction_file.is_file():
+                prediction_file.unlink()
+
+            # resize image to 299,299
             image = PILImg.open(origin_form)
-            image = image.resize((150, 150), PILImg.ANTIALIAS)
-            # sharpness image
-            image = PILImageEnhance.Sharpness(image)
-            image = image.enhance(1.3)
-            print("Debug", thumb_file)
-            image.save(thumb_file, 'JPEG')
+            image = image.resize((299, 299), PILImg.ANTIALIAS)
+            image.save(original_file, 'PNG')
+
+            prediction_array = predict(original_file)
+            predict_image = PILImg.fromarray(prediction_array)
+            predict_image.save(prediction_file, 'PNG')
+
+            data.update(title=userform['title'].value)
             data.update(origin_name=origin_name)
-            data.update(thumb_name=thumb_name)
+            data.update(folder_dir=folder)
+            data.update(thumb_name=predicted_name)
             userform = UploadForm()
     else:
         userform = UploadForm()
 
     data.update(userform=userform)
     return render(request, template, data)
-# class CreatePostView(CreateView):  # new
-#     model = Upload
-#     form_class = UploadForm
-#     template_name = 'uploads.html'
-#     success_url = reverse_lazy('home')
 
 
-# # Create your views here.
-# def index(request):
-#     # dests = Destination.objects.all
-#     return render(request, 'home.html')
-#     # return render(request, 'index.html', {'dests':dests})
-#
-# def upload(request):
-#     if request.method == 'POST':
-#         form = UploadForm(request.POST, request.FILES)
+
+
